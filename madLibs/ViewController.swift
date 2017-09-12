0a2855782cf553549c993cc2b0e7e800e224523b
Andrew Lau <andrew@Andrews-MacBook-Pro.local>
2017-09-12 11:24:01 -0700

    Initial Commit

---
+++ madLibs/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  madLibs
+//
+//  Created by Andrew Lau on 9/12/17.
+//  Copyright © 2017 Andrew Lau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
